fc67fa7a6feeca35f6b4ec11e919d0afb339a6c2
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-03-05 17:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded",function(){
+let ageval=document.getElementById("age");
+let nameval=document.getElementById("name");
+let form=document.getElementById("voteForm");
+
+	form.addEventListener("submit",function(e){
+		e.preventDefault();
+		let age=Number(ageval.value);
+		let name=nameval.value.trim();
+		if(age<=0 || name==="" || isNaN(age)){
+			alert("Please enter valid details.");
+			return;
+		}
+		new Promise((resolve,reject)=>{
+			if(age>=18){
+				setTimeout(()=>{
+					resolve("Welcome, . You can vote.");
+				},4000);
+			}
+			else{
+				reject("Oh sorry . You aren't old enough.");
+			}
+		})
+		.then((res)=>{
+			alert(res);
+		})
+		.catch((err)=>{
+			alert(err);
+		});
+});
+})
